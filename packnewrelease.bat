ae987c554daae7ebac1e58c3abb6b6f13560670a
Tore Aurstad <tore.aurstad@hemit.no>
2021-07-20 00:07:59 +0200

    note in bat file

--- packnewrelease.bat
+++ packnewrelease.bat
@@ -1,3 +1,4 @@
 echo Remember to bump the version attribute in DapperUtils.ToreAurstadIT.nuspec file !
+echo Remember to rebuild entire solution as Release first also !
 cd DapperUtils.ToreAurstadIT
 nuget pack -Prop Configuration=Release
